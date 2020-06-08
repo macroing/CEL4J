3aed3c760c6243491c0c2a94fcb03b7e42411a4c
macroing <joergen.lundgren@macroing.org>
2020-06-08 20:19:11 +0200

    Update Javadocs for the Java Decompiler API.

--- src/main/java/org/macroing/cel4j/java/decompiler/package-info.java
+++ src/main/java/org/macroing/cel4j/java/decompiler/package-info.java
@@ -1,28 +1,38 @@
 /**
  * Provides the Java Decompiler API.
  * <p>
- * To use this API, consider the following example. It demonstrates the simplest use case, where the default configuration parameters are used, the decompiled source code is displayed in standard output and no decompilation progress is shown.
+ * To use this API, consider the following examples.
+ * <p>
+ * The example below demonstrates the simplest use case, where the default configuration parameters are used, the decompiled source code is displayed in standard output and no decompilation progress is shown.
  * <pre>
- * {@code
- * Decompiler decompiler = Decompiler.newInstance();
- * decompiler.addClass(Integer.class);
- * decompiler.decompile();
+ * <code>
+ * try {
+ *     Decompiler decompiler = Decompiler.newInstance();
+ *     decompiler.addClass(Integer.class);
+ *     decompiler.decompile();
+ * } catch(DecompilationException e) {
+ *     e.printStackTrace();
  * }
+ * </code>
  * </pre>
- * The example below decompiles the source code to files, shows decompilation progress in standard output and changes some of the configuration parameters.
+ * The example below demonstrates more features, where some of the default configuration parameters are changed, the source code is decompiled to files and the decompilation progress is shown in standard output.
  * <pre>
- * {@code
- * Decompiler decompiler = Decompiler.newInstance();
- * decompiler.addClass(Integer.class, Consumers.file("generated", Integer.class));
- * decompiler.addClass(String.class, Consumers.file("generated", String.class));
- * decompiler.addDecompilerObserver(DecompilerObserver.print());
- * decompiler.getDecompilerConfiguration().setDisplayingAttributeInfos(true);
- * decompiler.getDecompilerConfiguration().setDisplayingConfigurationParameters(true);
- * decompiler.getDecompilerConfiguration().setDisplayingInstructions(true);
- * decompiler.getDecompilerConfiguration().setSeparatingGroups(true);
- * decompiler.getDecompilerConfiguration().setSortingGroups(true);
- * decompiler.decompile();
+ * <code>
+ * try {
+ *     Decompiler decompiler = Decompiler.newInstance();
+ *     decompiler.addClass(Integer.class, Consumers.file("generated", Integer.class));
+ *     decompiler.addClass(String.class, Consumers.file("generated", String.class));
+ *     decompiler.addDecompilerObserver(DecompilerObserver.print());
+ *     decompiler.getDecompilerConfiguration().setDisplayingAttributeInfos(true);
+ *     decompiler.getDecompilerConfiguration().setDisplayingConfigurationParameters(true);
+ *     decompiler.getDecompilerConfiguration().setDisplayingInstructions(true);
+ *     decompiler.getDecompilerConfiguration().setSeparatingGroups(true);
+ *     decompiler.getDecompilerConfiguration().setSortingGroups(true);
+ *     decompiler.decompile();
+ * } catch(DecompilationException e) {
+ *     e.printStackTrace();
  * }
+ * </code>
  * </pre>
  */
 package org.macroing.cel4j.java.decompiler;
