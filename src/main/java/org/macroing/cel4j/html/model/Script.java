4726d0691f909911c81f08f3ce9c38cee970aa52
macroing <joergen.lundgren@macroing.org>
2020-07-06 15:32:20 +0200

    Update the HTML Model API.

---
+++ src/main/java/org/macroing/cel4j/html/model/Script.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright 2009 - 2020 J&#246;rgen Lundgren
+ * 
+ * This file is part of org.macroing.cel4j.
+ * 
+ * org.macroing.cel4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * org.macroing.cel4j is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with org.macroing.cel4j. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.macroing.cel4j.html.model;
+
+import java.util.Objects;
+
+/**
+ * A {@code Script} represents a {@code script} element in HTML source code.
+ * <p>
+ * This class is mutable and not thread-safe.
+ * 
+ * @since 1.0.0
+ * @author J&#246;rgen Lundgren
+ */
+public final class Script extends ContentElement<Text> {
+	/**
+	 * The initial {@link Display} associated with a {@code Script} instance.
+	 */
+	public static final Display DISPLAY_INITIAL = Display.BLOCK;
+	
+	/**
+	 * The name associated with a {@code Script} instance.
+	 */
+	public static final String NAME = "script";
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	/**
+	 * Constructs a new {@code Script} instance.
+	 */
+	public Script() {
+		super(NAME, DISPLAY_INITIAL, new Text());
+	}
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	/**
+	 * Returns a {@code String} representation of this {@code Script} instance.
+	 * 
+	 * @return a {@code String} representation of this {@code Script} instance
+	 */
+	@Override
+	public String toString() {
+		return "new Script()";
+	}
+	
+	/**
+	 * Compares {@code object} to this {@code Script} instance for equality.
+	 * <p>
+	 * Returns {@code true} if, and only if, {@code object} is an instance of {@code Script}, and their respective values are equal, {@code false} otherwise.
+	 * 
+	 * @param object the {@code Object} to compare to this {@code Script} instance for equality
+	 * @return {@code true} if, and only if, {@code object} is an instance of {@code Script}, and their respective values are equal, {@code false} otherwise
+	 */
+	@Override
+	public boolean equals(final Object object) {
+		if(object == this) {
+			return true;
+		} else if(!(object instanceof Script)) {
+			return false;
+		} else if(!Objects.equals(getAttributes(), Script.class.cast(object).getAttributes())) {
+			return false;
+		} else if(!Objects.equals(getDisplay(), Script.class.cast(object).getDisplay())) {
+			return false;
+		} else if(!Objects.equals(getContent(), Script.class.cast(object).getContent())) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	/**
+	 * Returns a hash code for this {@code Script} instance.
+	 * 
+	 * @return a hash code for this {@code Script} instance
+	 */
+	@Override
+	public int hashCode() {
+		return Objects.hash(getAttributes(), getDisplay(), getContent());
+	}
+}
